dbdb151cc6fb241641a8e3bf04d74d991d925eb1
assemabuarab <100712119+assemabuarab@users.noreply.github.com>
2025-05-15 07:55:51 +0300

    Update download.php

--- download.php
+++ download.php
@@ -2,7 +2,7 @@
 // حماية إضافية ممكنة لاحقاً (مثل التحقق من الجلسة أو IP)
 
 // الرابط الحقيقي للتحميل
-$downloadUrl = 'https://dlll.apkm.app/instagold/9.90/InstaGold_v9.90.apk';
+$downloadUrl = 'https://dlll.apkm.app/instagold/9.95/InstaGold_v9.95.apk';
 
 // إعادة توجيه المستخدم إلى الرابط
 header("Location: $downloadUrl");
